0842abf1021ea6c709f9db7bd6afb8ffd46de50d
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-06-15 04:28:38 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,6 +6,31 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
 router.get("/me",verifyToken,getUser);
+router.get("/github-activity/:page", async (req, res) => {
+  const page = req.params.page || 1; // default to 1 if not provided
+
+  try {
+    const response = await fetch(`https://api.github.com/users/sarans-h/events?per_page=30&page=${page}`, {
+      headers: {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': `Bearer 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`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "GitHub API error", details: await response.text() });
+    }
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (err) {
+    console.error("GitHub proxy error:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // router.post("/google", google);
 export default router
 
